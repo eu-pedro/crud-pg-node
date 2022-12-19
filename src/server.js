e00c12a740d10a22519e822709f3d8bd2aacc69c
pedro <pedro.henrike2812@gmail.com>
2022-12-19 20:35:54 -0300

    adicionado o CORS

--- src/server.js
+++ src/server.js
@@ -5,6 +5,19 @@ const client = require('./database/config.js')
 const routes = require('./routes')
 
 
+const cors = require('cors');
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+
+
+
 client.connect();
 
 app.use(express.json())
